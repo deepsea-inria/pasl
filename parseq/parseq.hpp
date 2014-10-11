b96085859bab054e344ab48752fe88dba818c26e
Mike Rainey <mike.rainey@inria.fr>
2014-10-11 02:51:38 +0200

    ok

---
+++ parseq/parseq.hpp
@@ -0,0 +1,32 @@
+/* COPYRIGHT (c) 2014 Umut Acar, Arthur Chargueraud, and Michael
+ * Rainey
+ * All rights reserved.
+ *
+ * \file parseq.hpp
+ * \brief Parallel sequence operations
+ *
+ */
+
+#include "chunkedseq.hpp"
+#include "chunkedbag.hpp"
+#include "threaddag.hpp"
+
+#ifndef _PASL_DATA_PARSEQ_H_
+#define _PASL_DATA_PARSEQ_H_
+
+namespace pasl {
+namespace data {
+namespace parseq {
+  
+/***********************************************************************/
+  
+
+
+  
+/***********************************************************************/
+  
+}
+}
+}
+
+#endif /*! _PASL_DATA_PARSEQ_H_ */
