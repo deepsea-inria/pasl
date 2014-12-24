b80f3be601229b972761d66ac791196be6fcde59
Aksenov239 <aksenov.vitaly@gmail.com>
2014-12-24 19:06:22 +0300

    Adding the non-working code to the working standalon.

--- granularity/synthetic.cpp
+++ granularity/synthetic.cpp
@@ -12,22 +12,38 @@ using std::max;
 /*---------------------------------------------------------------------*/
 
 #ifdef CMDLINE
-  typedef control_by_cmdline control_type;
+  typedef pasl::sched::granularity::control_by_cmdline control_type;
 #elif PREDICTION
-  typedef control_by_prediction control_type;
+  typedef pasl::sched::granularity::control_by_prediction control_type;
 #elif CUTOFF_WITH_REPORTING
-  typename control_by_cutoff_with_reporting control_type;
+  typename pasl::sched::granularity::control_by_cutoff_with_reporting control_type;
 #elif CUTOFF_WITHOUT_REPORTING
-  typename control_by_cutoff_without_reporting control_type;
+  typename pasl::sched::granularity::control_by_cutoff_without_reporting control_type;
 #endif
+
+#ifdef BINARY
+  typedef pasl::sched::granularity::loop_by_eager_binary_splitting<control_type> loop_type;
+#elif LAZY_BINARY
+  typedef pasl::sched::granularity::loop_by_lazy_binary_splitting<control_type> loop_type;
+#elif SCHEDULING
+  typedef pasl::sched::granularity::loop_by_lazy_binary_splitting_scheduling<control_type> loop_type;
+#elif BINARY_WITH_SAMPLING
+  typedef pasl::sched::granularity::loop_control_with_sampling<loop_by_eager_binary_splitting<control_type>> loop_type;
+#elif BINARY_SEARCH
+  typedef pasl::sched::granularity::loop_by_binary_search_splitting<control_type> loop_type;
+#elif LAZY_BINARY_SEARCH
+  typedef pasl::sched::granularity::loop_by_lazy_binary_search_splitting<control_type> loop_type;
+#elif BINARY_SEARCH_WITH_SAMPLING
+  typedef pasl::sched::granularity::loop_control_with_sampling<loop_by_binary_search_splitting<control_type>> loop_type;
+#endif
+
 
 int p;
 int total = 0;
 void h() {
-  total++;
   int sum = 1;
   for (int i = 0; i < p; i++)
-    total ^= 1;
+    total++;
 }
 
 control_type cg("function g");
@@ -36,10 +52,10 @@ void g(int m) {
   if (m <= 1) {
     h();
   } else {
-    cstmt(cg,
+    pasl::sched::granularity::cstmt(cg,
       [&] { return m <= g_cutoff_const;},
       [&] { return m;},
-      [&] { fork2([&] {g(m / 2);}, [&] {g(m - m / 2);}); },
+      [&] { pasl::sched::granularity::fork2([&] {g(m / 2);}, [&] {g(m - m / 2);}); },
       [&] {
         for (int i = 0; i < m; i++) h();
 //        g(m / 2);
@@ -56,17 +72,47 @@ void f(int n, int m) {
   if (n <= 1) {
     g(m);
   } else {
-    cstmt(cf,
+    pasl::sched::granularity::cstmt(cf,
       [&] { return n * m <= f_cutoff_const;},
       [&] { return n * m;},
-      [&] { fork2([&] {f(n / 2, m);}, [&] {f(n - n / 2, m);}); },
+      [&] { pasl::sched::granularity::fork2([&] {f(n / 2, m);}, [&] {f(n - n / 2, m);}); },
       [&] {
-//        for (int i = 0; i < n; i++) g(m);
-        f(n / 2, m);
-        f(n - n / 2, m);
+        for (int i = 0; i < n; i++) g(m);
+//        f(n / 2, m);
+//        f(n - n / 2, m);
       }
     );
   }
+}
+
+
+loop_type sol_contr("synthetic outer loop");
+loop_type sil_contr("synthetic inner loop");
+                                
+void synthetic(int n, int m, int p) {
+#ifdef LITE
+  pasl::sched::granularity::parallel_for(sol_contr,
+    [&] (int L, int R) {return true;},        
+    [&] (int L, int R) {return (R - L) * m;},
+    int(0), n, [&] (int i) {
+      pasl::sched::granularity::parallel_for(sil_contr,
+        [&] (int L, int R) {return true;},
+        [&] (int L, int R) {return (R - L);},
+        int(0), m, [&] (int i) {
+          for (int k = 0; k < p; k++) {
+            total++;
+          }
+      });
+  });
+#elif STANDART
+  pasl::sched::native::parallel_for(int(0), n, [&] (int i) {
+      pasl::sched::native::parallel_for(int(0), m, [&] (int i) {
+          for (int k = 0; k < p; k++) {
+            total++;
+          }
+      });
+  });
+#endif
 }
 
 
@@ -74,15 +120,17 @@ void f(int n, int m) {
 
 void initialization() {
   pasl::util::ticks::set_ticks_per_seconds(1000);
-//  local_constants.init(undefined);
   cf.initialize(10000, 1);
   cg.initialize(1);
-  execmode.init(dynidentifier<execmode_type>());
+
+  sol_contr.initialize(1, 10);
+  sil_contr.initialize(1, 10);
 }
 
 int main(int argc, char** argv) {
   int n, m;
   std::string running_mode;
+  int type;
   
   auto init = [&] {
     initialization();
@@ -96,14 +144,23 @@ int main(int argc, char** argv) {
         "m", 2000);
     p = pasl::util::cmdline::parse_or_default_int(
         "p", 100);
+    type = pasl::util::cmdline::parse_or_default_int(
+        "type", 1);
     running_mode = pasl::util::cmdline::parse_or_default_string(
         "mode", std::string("by_force_sequential"));
     std::cout << "Using " << running_mode << " mode" << std::endl;;
+
     cf.set(running_mode);
     cg.set(running_mode);
+
+    sol_contr.set(running_mode);
+    sil_contr.set(running_mode);
   };
   auto run = [&] (bool sequential) {
-    f(n, m);
+    if (type == 1)
+      f(n, m);
+    else
+      synthetic(n, m, p);
   };
 
   auto output = [&] {
