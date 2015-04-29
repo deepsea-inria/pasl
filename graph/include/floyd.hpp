aad3af64f1f392e1625c461e57731d7cbf326747
Grigory Tkachenko <grtkachenko@gmail.com>
2015-04-29 03:53:10 +0300

    Skeleton for floyd_warshall_par_bfs_opt

--- graph/include/floyd.hpp
+++ graph/include/floyd.hpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <unordered_map>
 #include <stdio.h>
+#include "bellman_ford.hpp"
 
 #ifndef _PASL_GRAPH_FLOYD_H_
 #define _PASL_GRAPH_FLOYD_H_
@@ -48,9 +49,9 @@ namespace pasl {
           case FW_PAR_BFS:
             return floyd_warshall_par_bfs(graph);
             break;
-          case FW_PAR_BFSmemory:
-            return floyd_warshall_par_bfs_memory_opt(graph);
-            break;              
+          case FW_PAR_BFS_OPT:
+            return floyd_warshall_par_bfs_opt(graph);
+            break;
           case FW_PAR_BFS2:
             return floyd_warshall_par_bfs2(graph);
             break;
@@ -61,12 +62,15 @@ namespace pasl {
       
     private:
       using vtxid_type = typename adjlist<Adjlist_seq>::vtxid_type;
+      using adjlist_type = adjlist<Adjlist_seq>;
+      using adjlist_alias_type = typename adjlist_type::alias_type;      
+      using Frontier = pasl::graph::frontiersegbag<adjlist_alias_type>;
 
       enum FW_ALGO { 	
         FW_SERIAL_CLASSIC,
         FW_PAR_CLASSIC,      
         FW_PAR_BFS,
-        FW_PAR_BFSmemory,      
+        FW_PAR_BFS_OPT,      
         FW_PAR_BFS2,        
         FW_NB_ALGO 
       };   
@@ -75,7 +79,7 @@ namespace pasl {
         "SerialClassic", 
         "ParClassic",      
         "ParBFSForAllVertices",
-        "ParBFSForAllVertices (Memory Optimized)",
+        "ParBFSForAllVertices (with big vertex optimization)",
         "ParBFSForEveryVertex"
       };    
       
@@ -208,26 +212,141 @@ namespace pasl {
       /*---------------------------------------------------------------------*/
       /*---------------------------------------------------------------------*/
       /* Floyd-Warshall; parallel bfs */
-      /* One bfs for all the vertices */      
+      /* with big vertex optimization (WARNING: ONLY FOR UNDIRECTED UNWEIGHTED GRAPH) */      
       /*---------------------------------------------------------------------*/
-      static int* floyd_warshall_par_bfs_memory_opt(const adjlist<Adjlist_seq>& init_graph) {
+      
+      static inline bool try_to_update_mask() {
+        // TODO: write it
+        return true;
+      }
+          
+      template <class Adjlist_alias, class Frontier>
+      static void process_layer_par_lazy(const Adjlist_alias & graph_alias,
+                                         Frontier& frontier) {
+        
+        vtxid_type unknown = graph_constants<vtxid_type>::unknown_vtxid;
+        size_t nb_outedges = frontier.nb_outedges();
+        bool blocked = false;
+        while (nb_outedges > 0) {
+          if (nb_outedges <= bellman_ford_par_bfs_cutoff) {
+            blocked = true;
+            bellman_ford_algo<Adjlist_seq>::bfs_bellman_ford::reject();
+          }
+          if (bellman_ford_algo<Adjlist_seq>::bfs_bellman_ford::should_call_communicate()) {
+            if (nb_outedges > bellman_ford_par_bfs_cutoff) {
+              Frontier fr_in(graph_alias);
+              frontier.split(frontier.nb_outedges() / 2, fr_in);
+              sched::native::fork2([&] { process_layer_par_lazy(graph_alias, frontier); },
+                                   [&] { process_layer_par_lazy(graph_alias, fr_in); });                
+              if (blocked) // should always be false due to the order of the conditionals; yet, keep it for safety
+                bellman_ford_algo<Adjlist_seq>::bfs_bellman_ford::unblock(); 
+              return;
+            }
+            else
+            {
+              blocked = true;
+              bellman_ford_algo<Adjlist_seq>::bfs_bellman_ford::reject();
+            }
+          }
+          frontier.for_at_most_nb_outedges(communicate_cutoff, [&] (vtxid_type from, vtxid_type to, vtxid_type weight) {
+            // TODO: update mask
+          });
+          nb_outedges = frontier.nb_outedges();
+        }
+        if (blocked)
+          bellman_ford_algo<Adjlist_seq>::bfs_bellman_ford::unblock();
+      }
+          
+      static const int deep = 1;
+
+      static void calc_for_big_vertex(int big_vertex, const adjlist<Adjlist_seq>& graph, vtxid_type & nb_vertices, int* dists) {
+        int* num_vertices_at_level = data::mynew_array<int>(nb_vertices);
+        int* vertex_id_at_level = data::mynew_array<int>(nb_vertices);
+        int* level_offset = data::mynew_array<int>(nb_vertices);
+
+        fill_array_seq(vertex_id_at_level, nb_vertices, 0);
+        fill_array_seq(num_vertices_at_level, nb_vertices, 0);
+
+        const int off = big_vertex * nb_vertices;
+        for (int i = 0; i < nb_vertices; i++) {
+          int val = dists[off + i];
+          for (int j = val - deep; j <= val + deep; j++) {
+            if (0 <= j && j < nb_vertices) {
+              num_vertices_at_level[j]++;
+            }
+          }
+        }
+        level_offset[0] = 0;
+        for (int i = 1; i < nb_vertices; i++) {
+          level_offset[i] = level_offset[i - 1] + num_vertices_at_level[i - 1];
+        }
+        int* vertices_at_level = data::mynew_array<int>(level_offset[nb_vertices - 1] + num_vertices_at_level[nb_vertices - 1]);
+        for (int i = 0; i < nb_vertices; i++) {
+          int val = dists[off + i];
+          for (int j = val - deep; j <= val + deep; j++) {
+            if (0 <= j && j < nb_vertices) {            
+              vertices_at_level[level_offset[j] + vertex_id_at_level[j]] = i;
+              vertex_id_at_level[j]++;            
+            }
+          }
+        }
+        
+        for (int i = 0; i < nb_vertices; i++) {
+          std::cout << "Vertices at level " << i << std::endl; 
+          for (int j = 0; j < num_vertices_at_level[i]; j++) {
+            std::cout << vertices_at_level[level_offset[i] + j] << " ";          
+          }
+          std::cout << std::endl;
+        }
+        
+        auto graph_alias = get_alias_of_adjlist(graph);
+        Frontier frontier(graph_alias);
+        for (int layer = 0; layer < nb_vertices; layer++) {
+          frontier.clear();
+          for (int i = level_offset[layer]; i < level_offset[layer] + num_vertices_at_level[layer]; i++) {
+            frontier.push_vertex_back(vertices_at_level[i]);
+          }
+          process_layer_par_lazy(graph_alias, frontier);
+        }      
+      }
+        
+      
+      
+      static int* floyd_warshall_par_bfs_opt(const adjlist<Adjlist_seq>& graph) {
         using vtxid_type = typename adjlist<Adjlist_seq>::vtxid_type;
         
-        vtxid_type nb_vertices = init_graph.get_nb_vertices();
-        vtxid_type nb_edges = init_graph.nb_edges;
-        int vertices_to_process = nb_vertices;
-        if ((long long) nb_edges * nb_vertices > 1e9) 
-					vertices_to_process = std::min(nb_vertices, 1000000000 / nb_edges);
+        vtxid_type nb_vertices = graph.get_nb_vertices();
+        vtxid_type nb_edges = graph.nb_edges;
         
-        std::cout << "Vertices to process per round " << vertices_to_process << std::endl;
-
-        int* dists = data::mynew_array<int>((long long) nb_vertices * nb_vertices * 2);
-        auto graph = modify_graph(init_graph, vertices_to_process);
-        std::cout << "Finished modifiyng graph" << std::endl;
-        process(0, nb_vertices / vertices_to_process + 1, graph, nb_vertices, dists, vertices_to_process);
+        int max_deg = -1, max_num = -1;
+        for (int i = 0; i < nb_vertices; i++) {
+          vtxid_type degree = graph.adjlists[i].get_in_degree();
+          if (degree > max_deg) {
+            max_deg = degree;
+            max_num = i;
+          }
+        };
+        bool* used = data::mynew_array<bool>(nb_vertices);
+        fill_array_seq(used, nb_vertices, false);
+        vtxid_type degree = graph.adjlists[max_num].get_in_degree();
+        for (vtxid_type edge = 0; edge < degree; edge++) {
+          vtxid_type other = graph.adjlists[max_num].get_in_neighbor(edge);
+          used[other] = true;
+        }
+       	int num_calc = nb_vertices - max_deg;
+        std::cout << "Num calc " << num_calc << std::endl;
+        int* vertices_to_calc = data::mynew_array<int>(num_calc);
+        int vertices_to_calc_id = 0;
+        for (int i = 0; i < nb_vertices; i++) {
+          if (!used[i]) {
+            vertices_to_calc[vertices_to_calc_id++] = i;
+          }               
+        }
+        int* dists = data::mynew_array<int>((long long) nb_vertices * nb_vertices);
+        process(0, num_calc, graph, nb_vertices, dists, vertices_to_calc);
+        calc_for_big_vertex(max_num, graph, nb_vertices, dists);
         return dists;
       }  
-      
      
       
       /*---------------------------------------------------------------------*/
@@ -240,26 +359,21 @@ namespace pasl {
         using vtxid_type = typename adjlist<Adjlist_seq>::vtxid_type;
         vtxid_type nb_vertices = init_graph.get_nb_vertices();
         int* dists = data::mynew_array<int>((long long) nb_vertices * nb_vertices);
-        int vertices_to_process = 1;
-        process(0, nb_vertices, init_graph, nb_vertices, dists, vertices_to_process);
+        process(0, nb_vertices, init_graph, nb_vertices, dists);
         return dists;
       } 
       
-      static void process(int index_from, int index_to, const adjlist<Adjlist_seq>& graph, vtxid_type & nb_vertices, int* dists, int & vertices_to_process) {
+      static void process(int index_from, int index_to, const adjlist<Adjlist_seq>& graph, vtxid_type & nb_vertices, int* dists, int * vertices_to_calc = nullptr) {
         if (index_to - index_from < 1000) {
           for (int i = index_from; i < index_to; ++i) {
-            std::vector<vtxid_type> sources;
-            int from = i * vertices_to_process, to = std::min(nb_vertices, from + vertices_to_process);
-            std::cout << from << " " << to << std::endl;
-            for (int j = from; j < to; ++j) {
-              sources.push_back((j - from) * nb_vertices + j);
-            }
-            bellman_ford_algo<Adjlist_seq>::bfs_bellman_ford::bellman_ford_par_bfs(graph, sources, false, dists + (long long) from * nb_vertices); 
+            int cur_vertex = i;
+            if (vertices_to_calc != nullptr) cur_vertex = vertices_to_calc[i];
             
+            bellman_ford_algo<Adjlist_seq>::bfs_bellman_ford::bellman_ford_par_bfs(graph, cur_vertex, false, dists + (long long) cur_vertex * nb_vertices);             
           }
         } else {
-          sched::native::fork2([&] { process(index_from, (index_from + index_to) / 2, graph, nb_vertices, dists, vertices_to_process); },
-                               [&] { process((index_from + index_to) / 2, index_to, graph, nb_vertices, dists, vertices_to_process); });
+          sched::native::fork2([&] { process(index_from, (index_from + index_to) / 2, graph, nb_vertices, dists); },
+                               [&] { process((index_from + index_to) / 2, index_to, graph, nb_vertices, dists); });
         }
         
       }
