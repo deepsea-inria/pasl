ab3497d6d08d8a4ae560c9fd5f2539f9779fb601
Mike Rainey <mike.rainey@inria.fr>
2015-11-03 17:57:38 +0100

    Use just trivial synthetic workload in synthetic benchmarks

--- dag-benchmarks/bench.ml
+++ dag-benchmarks/bench.ml
@@ -581,7 +581,8 @@ let prog = "./bench.opt"
 let make() =
   build "." [prog] arg_virtual_build
 
-let mk_workloads = mk_list int "workload" (XList.init 4 (fun i -> i * 500))
+let mk_workloads = mk_list int "workload" [0;]
+(*(XList.init 4 (fun i -> i * 500)) *)
         
 let mk_all_benchmarks =
 (*     mk_incounter_mixed_duration
